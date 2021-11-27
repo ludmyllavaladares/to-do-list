67a4cd6c8cb7e6d6786ad0826dfa20b11bf92b20
ludmyllavaladares <valadaresludmylla@gmail.com>
2021-11-27 10:57:44 -0300

    estilizando o app

---
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+//Interface
+import { TaskList } from '../../model/task-list';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+
+  public taskList: Array<TaskList> = [
+    {task: "Minha nova Task", checked: true},
+    {task: "Minha nova Task2", checked: false},
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+
+}
